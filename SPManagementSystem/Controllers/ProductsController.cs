a816acc8deffd7c15e3a9738b359e01c3fd03e38
Belu <belena0195ba@gmail.com>
2024-03-17 10:29:06 -0400

    Adding products and view model

---
+++ SPManagementSystem/Controllers/ProductsController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SPManagementSystem.Models;
+using SPManagementSystem.ViewModels;
+
+namespace SPManagementSystem.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            var products = ProductsRepository.GetProducts(loadCategory:true);
+            return View(products);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            ViewBag.Action = "edit";
+            var product = ProductsRepository.GetProductById(id.HasValue ? id.Value : 0);
+            return View(product);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                ProductsRepository.UpdateProduct(product.ProductId, product);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(product);
+        }
+        public IActionResult Add()
+        {
+            ViewBag.Action = "add";
+            var productViewModel = new ProductViewModel
+            {
+                Categories = CategoriesRepository.GetCategories()
+            };
+            return View(productViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Add(ProductViewModel productViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                ProductsRepository.AddProduct(productViewModel.Product);
+                return RedirectToAction(nameof(Index));
+            }
+            productViewModel.Categories = CategoriesRepository.GetCategories(); 
+            return View(productViewModel);
+        }
+
+        public IActionResult Delete(int productId)
+        {
+            ProductsRepository.DeleteProduct(productId);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ SPManagementSystem/Models/ProductsRepository.cs
@@ -0,0 +1,85 @@
+﻿namespace SPManagementSystem.Models
+{
+    public class ProductsRepository
+    {
+        private static List<Product> _products = new List<Product>()
+        {
+            new Product { ProductId = 1, CategoryId = 1, Name = "Iced Tea", Quantity = 100, Price = 1.99 },
+            new Product { ProductId = 2, CategoryId = 1, Name = "Canada Dry", Quantity = 200, Price = 1.99 },
+            new Product { ProductId = 3, CategoryId = 2, Name = "Whole Wheat Bread", Quantity = 300, Price = 1.50 },
+            new Product { ProductId = 4, CategoryId = 2, Name = "White Bread", Quantity = 300, Price = 1.50 }
+        };
+
+        public static void AddProduct(Product product)
+        {
+            var maxId = _products.Max(x => x.ProductId);
+            product.ProductId = maxId;
+            _products.Add(product);
+        }
+
+        public static List<Product> GetProducts(bool loadCategory = false)
+        {
+            if (!loadCategory)
+            {
+                return _products;
+            } 
+            else
+            {
+                if (_products != null && _products.Count > 0)
+                {
+                    _products.ForEach(x =>
+                    {
+                        if (x.CategoryId.HasValue)
+                            x.Category = CategoriesRepository.GetCategoryById(x.CategoryId.Value);
+                    });
+                }
+                return _products ?? new List<Product>();
+            }
+        }
+
+        public static Product? GetProductById(int productId, bool loadCategory = false)
+        {
+            var product = _products.FirstOrDefault(x => x.ProductId == productId);
+            if (product != null)
+            {
+                var prod = new Product
+                {
+                    ProductId = product.ProductId,
+                    Name = product.Name,
+                    Quantity = product.Quantity,
+                    Price = product.Price,
+                    CategoryId = product.CategoryId,
+                };
+
+                if (loadCategory && prod.CategoryId.HasValue)
+                {
+                    prod.Category = CategoriesRepository.GetCategoryById(prod.CategoryId.Value);
+                }
+            }
+            return null;
+        }
+
+        public static void UpdateProduct(int productId, Product product)
+        {
+            if (productId != product.ProductId) return;
+
+            var productoUpdate = _products.FirstOrDefault(x => x.ProductId == productId);
+            if (productoUpdate != null)
+            {
+                productoUpdate.Name = product.Name; 
+                productoUpdate.Quantity = product.Quantity; 
+                productoUpdate.Price = product.Price;
+                productoUpdate.CategoryId = product.CategoryId; 
+            }
+        }
+
+        public static void DeleteProduct(int productId)
+        {
+            var product = _products.FirstOrDefault(x => x.ProductId == productId);
+            if (product != null)
+            {
+                _products.Remove(product);
+            }
+        }
+    }
+}
