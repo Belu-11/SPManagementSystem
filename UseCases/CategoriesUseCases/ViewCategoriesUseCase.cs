cc813691af585570915ff2d68ed9724669a42170
Belu <belena0195ba@gmail.com>
2024-05-02 16:51:52 -0400

    implementing clean architecture: added two projects: onefor entities and one for use cases. Use cases will use plugging based clean architecture

---
+++ UseCases/CategoriesUseCases/ViewCategoriesUseCase.cs
@@ -0,0 +1,23 @@
+﻿using CoreBusiness;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UseCases.CategoriesUseCases
+{
+    public class ViewCategoriesUseCase
+    {
+        private readonly ICategryRepository categryRepository;
+
+        public ViewCategoriesUseCase(ICategryRepository categryRepository)
+        {
+            this.categryRepository = categryRepository;
+        }
+        public IEnumerable<Category> Execute()
+        {
+            return categryRepository.GetCategories();
+        }
+    }
+}
