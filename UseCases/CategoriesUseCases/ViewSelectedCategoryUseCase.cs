bc708ca9ad37daa616da73b7fc5aa4038ce6b51f
Belu <belena0195ba@gmail.com>
2024-05-04 16:41:06 -0400

    - fixed method signature for GetCategoryById on the interface ICategoryRepository - created new project to store plugins - implemented ICategoryRepository for in memory data storage in a new pluging (Pluging.DataStore.InMemory)

--- UseCases/CategoriesUseCases/ViewSelectedCategoryUseCase.cs
+++ UseCases/CategoriesUseCases/ViewSelectedCategoryUseCase.cs
@@ -15,7 +15,7 @@ namespace UseCases.CategoriesUseCases
         {
             this.categoryRepository = categoryRepository;
         }
-        public IEnumerable<Category> Execute(int categoryId)
+        public Category? Execute(int categoryId)
         {
             return categoryRepository.GetCategoryById(categoryId);
 
