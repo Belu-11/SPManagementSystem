9851cdd3353cdcdba22229e78620540c23036c5a
Belu <belena0195ba@gmail.com>
2024-06-03 12:08:53 -0400

    fix

--- SPManagementSystem/Areas/Identity/Data/AccountContext.cs
+++ SPManagementSystem/Areas/Identity/Data/AccountContext.cs
@@ -5,7 +5,6 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using SPManagementSystem.Areas.Identity.Data;
 
 namespace SPManagementSystem.Data
 {
